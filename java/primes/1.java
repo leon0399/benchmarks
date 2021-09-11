99e4a28ed30ab4342cdd3db9489dfd4e67fbc46c
Leonid Meleshin <leon.03.99@gmail.com>
2021-09-11 21:10:45 +0300

    feat: add Java Atkin sieve

---
+++ java/primes/1.java
@@ -0,0 +1,145 @@
+import java.util.AbstractMap.SimpleEntry;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.BitSet;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+
+public final class Primes {
+    private static final int UPPER_BOUND = 5000000;
+    private static final int PREFIX = 32338;
+
+    static final class Node {
+        public final Map<Character, Node> children = new HashMap<>();
+        public boolean terminal = false;
+    }
+
+    static final class Sieve {
+        private final int limit;
+        private final BitSet prime;
+
+        Sieve(final int limit) {
+            this.limit = limit;
+            this.prime = new BitSet(limit + 1);
+        }
+
+        Iterable<Integer> toList() {
+            final var result = new ArrayList<Integer>(Arrays.asList(2, 3));
+            for (var p = 5; p <= this.limit; ++p) {
+                if (this.prime.get(p)) {
+                    result.add(p);
+                }
+            }
+            return result;
+        }
+
+        Sieve omitSquares() {
+            for (var r = 5; r * r < this.limit; ++r) {
+                if (this.prime.get(r)) {
+                    for (var i = r * r; i < this.limit; i += r * r) {
+                        this.prime.clear(i);
+                    }
+                }
+            }
+            return this;
+        }
+
+        void step1(int x, int y) {
+            var n = (4 * x * x) + (y * y);
+            if (n <= this.limit && (n % 12 == 1 || n % 12 == 5)) {
+                this.prime.flip(n);
+            }
+        }
+
+        void step2(int x, int y) {
+            var n = (3 * x * x) + (y * y);
+            if (n <= this.limit && n % 12 == 7) {
+                this.prime.flip(n);
+            }
+        }
+
+        void step3(int x, int y) {
+            var n = (3 * x * x) - (y * y);
+            if (x > y && n <= this.limit && n % 12 == 11) {
+                this.prime.flip(n);
+            }
+        }
+
+        void loopY(int x) {
+            for (var y = 1; y * y < limit; ++y) {
+                this.step1(x, y);
+                this.step2(x, y);
+                this.step3(x, y);
+            }
+        }
+
+        void loopX() {
+            for (var x = 1; x * x < limit; ++x) {
+                this.loopY(x);
+            }
+        }
+
+        Sieve calc() {
+            this.loopX();
+            return this.omitSquares();
+        }
+    }
+
+    private static Node generateTree(final Iterable<Integer> l) {
+        var root = new Node();
+        for (final var el : l) {
+            var head = root;
+            for (final var ch : String.valueOf(el).toCharArray()) {
+                if (!head.children.containsKey(ch)) {
+                    head.children.put(ch, new Node());
+                }
+                head = head.children.get(ch);
+            }
+            head.terminal = true;
+        }
+        return root;
+    }
+
+    private static Iterable<Integer> find(int upperBound, int prefix) {
+        final var primes = new Sieve(upperBound).calc();
+        final var strPrefix = String.valueOf(prefix);
+        var head = generateTree(primes.toList());
+        for (final var ch : strPrefix.toCharArray()) {
+            if (head.children.containsKey(ch)) {
+                head = head.children.get(ch);
+            } else {
+                return null;
+            }
+        }
+        final Queue<Map.Entry<Node, String>> queue = new LinkedList<>(
+            Arrays.asList(new SimpleEntry<Node, String>(head, strPrefix))
+        );
+
+        final List<Integer> result = new ArrayList<>();
+
+        while (queue.peek() != null) {
+            final var queueTop = queue.poll();
+            final var top = queueTop.getKey();
+            final var currentPrefix = queueTop.getValue();
+            if (top.terminal) {
+                result.add(Integer.valueOf(currentPrefix));
+            }
+            for (final var entry : top.children.entrySet()) {
+                queue.add(
+                    new SimpleEntry<Node, String>(
+                        entry.getValue(),
+                        currentPrefix + entry.getKey()
+                    )
+                );
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(find(UPPER_BOUND, PREFIX));
+    }
+}

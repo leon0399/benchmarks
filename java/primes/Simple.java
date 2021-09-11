24834caaa88a47eed1580c8c28524ade1c3d8507
Leonid Meleshin <leon.03.99@gmail.com>
2021-09-11 22:02:20 +0300

    chore: use friendly names

--- java/primes/Simple.java
+++ java/primes/Simple.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 
-public final class Primes {
+public final class Simple {
     private static final int UPPER_BOUND = 5000000;
     private static final int PREFIX = 32338;
 

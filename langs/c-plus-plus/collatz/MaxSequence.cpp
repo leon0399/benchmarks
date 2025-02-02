6fcd612dc0ae5830d03b0375902a24fd379f39c1
Leonid Meleshin <hello@leon0399.ru>
2025-02-02 18:18:38 +0100

    fix(C++): prevent undefined behavior for Collatz
    Close #1

--- langs/c-plus-plus/collatz/MaxSequence.cpp
+++ langs/c-plus-plus/collatz/MaxSequence.cpp
@@ -4,8 +4,8 @@
 
 static const auto NUMBER = 500000;
 
-int collatz(int x) {
-    int len = 0;
+unsigned long long collatz(unsigned long long x) {
+    unsigned long long len = 0;
 
     while (x > 1) {
         if (x % 2 == 0) {
@@ -13,21 +13,18 @@ int collatz(int x) {
         } else {
             x = 3 * x + 1;
         }
-
         len++;
     }
-
     return len;
 }
 
-std::pair<int, int> findMaxCollatz(int to) {
-    std::pair<int, int> result = std::make_pair(1, 1);
+std::pair<int, unsigned long long> findMaxCollatz(int to) {
+    std::pair<int, unsigned long long> result = {1, 1};
 
     for (int number = 1; number <= to; number++) {
-        auto len = collatz(number);
-
+        auto len = collatz(static_cast<unsigned long long>(number));
         if (len > result.second) {
-            result = std::make_pair(number, len);
+            result = {number, len};
         }
     }
 
@@ -36,12 +33,13 @@ std::pair<int, int> findMaxCollatz(int to) {
 
 int main() {
     const auto start_time = std::chrono::high_resolution_clock::now();
-    
+
     const auto result = findMaxCollatz(NUMBER);
     std::cout << "[" << result.first << ", " << result.second << "]" << std::endl;
-    
+
     const auto end_time = std::chrono::high_resolution_clock::now();
     const auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
 
     std::cout << "Execution time: " << duration << "ms" << std::endl;
+    return 0;
 }
